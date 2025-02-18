a86931441b77327d501b38f615dee05de9640671
yheseul <kangyheseul@naver.com>
2025-02-18 20:33:30 +0900

    feat: implement lauout banner component

---
+++ src/commons/layout/LayoutBanner.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import BannerSlider from "../../app/_components/BannerSlider";
+
+export default function LayoutBanner() {
+  return (
+    <BannerSlider
+      delay={3000}
+      length={6}
+      image="banner"
+      width="100%"
+      height="512px"
+    />
+  );
+}
