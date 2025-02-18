af5ac56a347c999f13e9f29cef03ff52db0e83f3
yheseul <kangyheseul@naver.com>
2025-02-18 21:32:23 +0900

    feat: implement filter component with text and date

---
+++ src/app/_components/FilterBar.tsx
@@ -0,0 +1,36 @@
+import { usePathname } from "next/navigation";
+import Button from "./Button";
+import CustomDatePicker from "./CustomDatePicker";
+import SearchBar from "./SearchBar/SearchBar";
+import { useRouter } from "next/navigation";
+
+const FilterBar = () => {
+  const path = usePathname();
+  const isPurchasePage = path.includes("purchase");
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    if (isPurchasePage) router.push("#");
+    else router.push("/boards/new");
+  };
+
+  return (
+    <div className="flex justify-between">
+      <div className="flex gap-3 w-[1000px]">
+        <CustomDatePicker />
+        <SearchBar />
+        <Button color="black" id="search" width="100px" />
+      </div>
+      <div>
+        <Button
+          color="blue"
+          id={isPurchasePage ? "sale" : "register"}
+          width="150px"
+          onClick={handleNavigate}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FilterBar;
