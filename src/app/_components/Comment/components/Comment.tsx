0da2006f642060ea9485785ba7770bfd09cc90f7
yheseul <kangyheseul@naver.com>
2025-02-19 11:27:20 +0900

    style: implement comment item component

--- src/app/_components/Comment/components/Comment.tsx
+++ src/app/_components/Comment/components/Comment.tsx
@@ -1,9 +1,7 @@
-import StarRating from "../../StarRating/StarRating";
 import CommentEdit from "./CommentEdit";
-import { CloseOutlined, EditOutlined, UserOutlined } from "@ant-design/icons";
-import CreatedAt from "../../CreatedAt";
 import { ICommentList } from "../types/comment.type";
 import useComment from "../hooks/useComment";
+import CommentItem from "./CommentItem";
 
 export default function Comment(props: ICommentList) {
   const { onClickEditComment, onClickDeleteComment, commentData, isEdit } =
@@ -12,33 +10,11 @@ export default function Comment(props: ICommentList) {
   return (
     <li>
       {!isEdit ? (
-        <div className="flex flex-col w-full gap-2 py-10 border-b border-lightGray">
-          <div className="flex items-center justify-between w-full">
-            <div className="flex gap-2">
-              <div className="flex items-center gap-1 text-sm font-light text-neutralGray">
-                <UserOutlined />
-                <span>{commentData.writer}</span>
-              </div>
-              <div className="flex gap-0">
-                <StarRating defaultValue={commentData.rating} />
-              </div>
-            </div>
-            <div className="flex gap-2">
-              <EditOutlined
-                className="cursor-pointer"
-                onClick={onClickEditComment}
-              />
-              <CloseOutlined
-                className="cursor-pointer"
-                onClick={onClickDeleteComment}
-              />
-            </div>
-          </div>
-          <div>{commentData.contents}</div>
-          <span className="text-sm font-normal text-grayMuted">
-            <CreatedAt createdAt={commentData.createdAt ?? ""} />
-          </span>
-        </div>
+        <CommentItem
+          commentData={commentData}
+          onClickEditComment={onClickEditComment}
+          onClickDeleteComment={onClickDeleteComment}
+        />
       ) : (
         <div className="flex flex-col w-full gap-2 py-10 border-b border-lightGray">
           <CommentEdit commentData={commentData} />
---
+++ src/app/_components/Comment/components/CommentItem.tsx
@@ -0,0 +1,40 @@
+import { CloseOutlined, EditOutlined, UserOutlined } from "@ant-design/icons";
+import StarRating from "../../StarRating/StarRating";
+import CreatedAt from "../../CreatedAt";
+import { ICommentItemProps } from "../types/comment.type";
+
+const CommentItem = (props: ICommentItemProps) => {
+  const { commentData, onClickEditComment, onClickDeleteComment } = props;
+  
+  return (
+    <div className="flex flex-col w-full gap-2 py-10 border-b border-lightGray">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex gap-2">
+          <div className="flex items-center gap-1 text-sm font-light text-neutralGray">
+            <UserOutlined />
+            <span>{commentData.writer}</span>
+          </div>
+          <div className="flex gap-0">
+            <StarRating defaultValue={commentData.rating} />
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <EditOutlined
+            className="cursor-pointer"
+            onClick={onClickEditComment}
+          />
+          <CloseOutlined
+            className="cursor-pointer"
+            onClick={onClickDeleteComment}
+          />
+        </div>
+      </div>
+      <div>{commentData.contents}</div>
+      <span className="text-sm font-normal text-grayMuted">
+        <CreatedAt createdAt={commentData.createdAt ?? ""} />
+      </span>
+    </div>
+  );
+};
+
+export default CommentItem;
