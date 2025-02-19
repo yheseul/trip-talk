1de6e7ec4fee80d390596ba3fd3fffde34beed65
yheseul <kangyheseul@naver.com>
2025-02-19 16:37:51 +0900

    feat: define fetch api

---
+++ src/commons/backend-api.tsx
@@ -0,0 +1,178 @@
+import { gql } from "@apollo/client";
+
+export const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int, $search: String) {
+    fetchBoards(page: $page, search: $search) {
+      _id
+      writer
+      title
+      createdAt
+      youtubeUrl
+    }
+  }
+`;
+
+export const DELETE_BOARD = gql`
+  mutation deleteBoard($id: ID!) {
+    deleteBoard(boardId: $id)
+  }
+`;
+
+export const CREATE_BOARD = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      _id
+      writer
+      title
+      contents
+      createdAt
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+      youtubeUrl
+      likeCount
+      dislikeCount
+      images
+    }
+  }
+`;
+
+export const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      writer
+      title
+      contents
+      createdAt
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+      youtubeUrl
+      likeCount
+      dislikeCount
+      images
+    }
+  }
+`;
+
+export const UPDATE_BOARD = gql`
+  mutation updateBoard(
+    $updateBoardInput: UpdateBoardInput!
+    $password: String
+    $boardId: ID!
+  ) {
+    updateBoard(
+      updateBoardInput: $updateBoardInput
+      password: $password
+      boardId: $boardId
+    ) {
+      _id
+      title
+      contents
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+      youtubeUrl
+      likeCount
+      dislikeCount
+      images
+    }
+  }
+`;
+
+export const CREATE_BOARD_COMMENTS = gql`
+  mutation createBoardComment(
+    $createBoardCommentInput: CreateBoardCommentInput!
+    $boardId: ID!
+  ) {
+    createBoardComment(
+      createBoardCommentInput: $createBoardCommentInput
+      boardId: $boardId
+    ) {
+      _id
+      writer
+      contents
+      rating
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const FETCH_BOARD_COMMENTS = gql`
+  query fetchBoardComments($page: Int, $boardId: ID!) {
+    fetchBoardComments(page: $page, boardId: $boardId) {
+      _id
+      writer
+      contents
+      rating
+      createdAt
+    }
+  }
+`;
+
+export const FETCH_BOARDS_COUNT = gql`
+  query fetchBoardsCount {
+    fetchBoardsCount
+  }
+`;
+
+export const UPDATE_BOARD_COMMENT = gql`
+  mutation updateBoardComment(
+    $updateBoardCommentInput: UpdateBoardCommentInput!
+    $password: String
+    $boardCommentId: ID!
+  ) {
+    updateBoardComment(
+      updateBoardCommentInput: $updateBoardCommentInput
+      password: $password
+      boardCommentId: $boardCommentId
+    ) {
+      _id
+      writer
+      contents
+      rating
+      updatedAt
+    }
+  }
+`;
+
+export const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      accessToken
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation createUser($email: String!, $name: String!, $password: String!) {
+    createUser(
+      createUserInput: { email: $email, name: $name, password: $password }
+    ) {
+      _id
+    }
+  }
+`;
+
+export const RESTORE_ACCESS_TOKEN = gql`
+  mutation restoreAccessToken {
+    restoreAccessToken {
+      accessToken
+    }
+  }
+`;
