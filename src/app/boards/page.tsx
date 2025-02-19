cdecec34855ee62da7f8dcadf0dc525353290d1a
yheseul <kangyheseul@naver.com>
2025-02-19 15:09:47 +0900

    style: change CSS styles to inline

--- src/app/boards/page.tsx
+++ src/app/boards/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useQuery } from "@apollo/client";
-import styles from "./styles.module.css";
 import { FetchBoardsDocument } from "../../commons/graphql/graphql";
 import Pagination from "../_components/Pagination";
 import FilterBar from "../_components/FilterBar";
@@ -23,14 +22,14 @@ export default function Boards() {
   return (
     <div className="px-4">
       <FilterBar />
-      <div className={styles.post_contain}>
-        <div className={styles.boards_header}>
-          <div className={styles.boards_header_number}>번호</div>
-          <div className={styles.boards_header_title}>제목</div>
-          <div className={styles.boards_header_writer}>작성자</div>
-          <div className={styles.boards_header_createdAt}>날짜</div>
+      <div className="flex flex-col items-start self-stretch w-full gap-2 px-12 py-6 bg-white rounded-2xl min-w-[680px] shadow-[0px_0px_20px_0px_rgba(0,0,0,0.08)]">
+        <div className="flex items-center self-stretch gap-2 px-6 py-4 text-base font-medium text-charcoal">
+          <div className="flex w-16 items-center justify-center gap-2.5">번호</div>
+          <div className="flex gap-2.5 items-center justify-start flex-1">제목</div>
+          <div className="flex justify-center items-center gap-2.5">작성자</div>
+          <div className="flex justify-center items-center gap-2.5">날짜</div>
         </div>
-        <ul className={styles.posts}>
+        <ul className="flex flex-col w-full gap-2">
           {isDateRange
             ? data?.fetchBoards.map((board, index: number) => (
                 <Board
