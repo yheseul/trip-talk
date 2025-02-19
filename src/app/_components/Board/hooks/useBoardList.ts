6bd7b35e53ad9ebb2a00b14951a805861fbcc8a9
yheseul <kangyheseul@naver.com>
2025-02-19 12:42:12 +0900

    feat: implement delete and routing functionality for post list page

---
+++ src/app/_components/Board/hooks/useBoardList.ts
@@ -0,0 +1,30 @@
+import { useMutation } from "@apollo/client";
+import {
+  DeleteBoardDocument,
+  FetchBoardsDocument,
+} from "../../../../commons/graphql/graphql";
+import { useRouter } from "next/navigation";
+
+export default function useBoardsList(id: string) {
+  const router = useRouter();
+  const [deleteBoard] = useMutation(DeleteBoardDocument);
+
+  const onClickBoard = () => {
+    router.push(`/boards/${id}`);
+  };
+
+  const onCLickDelete = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    deleteBoard({
+      variables: {
+        id,
+      },
+      refetchQueries: [{ query: FetchBoardsDocument }],
+    });
+  };
+
+  return {
+    onClickBoard,
+    onCLickDelete,
+  };
+}
