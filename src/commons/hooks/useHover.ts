e34ea78a84641a2be6ff36efe29afa8b5f01814b
yheseul <kangyheseul@naver.com>
2025-02-19 16:47:07 +0900

    feat: create custom hover hook for displaying icon on mouse hover

---
+++ src/commons/hooks/useHover.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+
+const useHover = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseOver = () => setIsHovered(true);
+  const handleMouseOut = () => setIsHovered(false);
+
+  return { isHovered, handleMouseOver, handleMouseOut };
+};
+
+export default useHover;
