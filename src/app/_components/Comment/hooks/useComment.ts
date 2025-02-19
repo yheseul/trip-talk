85cad75944d68468a974961476a2e6d1bc5037e5
yheseul <kangyheseul@naver.com>
2025-02-19 11:31:10 +0900

    feat: enable comment editing on edit button click

---
+++ src/app/_components/Comment/hooks/useComment.ts
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { ICommentList } from "../types/comment.type";
+
+export default function useComment(commentData: ICommentList) {
+  // const { writer, contents, createdAt } = props;
+  const [isEdit, setIsEdit] = useState(false);
+
+  const onClickEditComment = () => {
+    setIsEdit(true);
+  };
+
+  const onClickDeleteComment = () => {
+    // delete!!
+  };
+
+  return {
+    onClickEditComment,
+    onClickDeleteComment,
+    commentData,
+    isEdit,
+  };
+}
