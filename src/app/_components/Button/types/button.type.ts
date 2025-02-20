c8e1a09bbf5f7a389fec5b4aa7177facc90805b4
yheseul <kangyheseul@naver.com>
2025-02-20 10:40:36 +0900

    types: define type for button component

---
+++ src/app/_components/Button/types/button.type.ts
@@ -0,0 +1,9 @@
+import { MouseEventHandler } from "react";
+
+export interface IButtonProps {
+  id: string;
+  disabled?: boolean;
+  color: "blue" | "gray" | "white" | "black";
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  width?: string;
+}
