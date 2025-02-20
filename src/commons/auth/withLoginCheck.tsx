bad8c4ea70f600c3d8f73dc164aa063d7a1697de
yheseul <kangyheseul@naver.com>
2025-02-20 11:01:31 +0900

    feat: implement higher-order component for login check

---
+++ src/commons/auth/withLoginCheck.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+import { useLoadStore } from "../stores/useLoadStore";
+import { useAccessTokenStore } from "../stores/useAccessTokenStore";
+import { useRouter } from "next/navigation";
+
+const withLoginCheck =
+  (Component: () => JSX.Element) =>
+  <P extends object>(props: P) => {
+    const router = useRouter();
+    const { accessToken } = useAccessTokenStore();
+    const { isLoaded } = useLoadStore();
+    useEffect(() => {
+      if (!isLoaded) return;
+      if (accessToken) return;
+
+      alert("로그인 후 다시 시도해 주세요.");
+      router.push("/");
+    }, [isLoaded]);
+    return <Component {...props} />;
+  };
+
+export default withLoginCheck;
