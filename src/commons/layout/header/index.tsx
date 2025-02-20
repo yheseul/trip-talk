36b66621e0aed6fc2306b399f385a65098cd0e37
yheseul <kangyheseul@naver.com>
2025-02-20 09:18:58 +0900

    style: implement layout header componenet

---
+++ src/commons/layout/header/index.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Image from "next/image";
+import { HEADER } from "../../../constants/constants";
+import { CaretDownOutlined, UserOutlined } from "@ant-design/icons";
+import useLayoutHeader from "./hooks/useLayoutHeader";
+
+export default function Header() {
+  const {
+    onClickTripTalk,
+    onClickMoveMainPage,
+    onClickMovePurchasePage,
+    onClickMoveMyPage,
+    mainPage,
+    purchasePage,
+    myPage,
+  } = useLayoutHeader();
+
+  return (
+    <div className="flex flex-row justify-between p-5">
+      <div className="flex flex-row items-center gap-6">
+        <div
+          onClick={onClickMoveMainPage}
+          className="relative w-[51.521px] h-8 p-2"
+        >
+          <Image src="/webp/logo.webp" alt="logo" fill />
+        </div>
+        <div
+          onClick={onClickTripTalk}
+          className={
+            mainPage
+              ? "text-black border-solid border-b-2 p-2 cursor-pointer"
+              : "text-gray p-2 cursor-pointer"
+          }
+        >
+          {HEADER.TRIP_TALK}
+        </div>
+        <div
+          onClick={onClickMovePurchasePage}
+          className={
+            purchasePage
+              ? "text-black border-solid border-b-2 p-2 cursor-pointer"
+              : "text-gray p-2 cursor-pointer"
+          }
+        >
+          {HEADER.PURCHASE_ACCOMMODATION_TICKETS}
+        </div>
+        <div
+          onClick={onClickMoveMyPage}
+          className={
+            myPage
+              ? "text-black border-solid border-b-2 p-2 cursor-pointer"
+              : "text-gray p-2 cursor-pointer"
+          }
+        >
+          {HEADER.MY_PAGE}
+        </div>
+      </div>
+      <div className="flex flex-row gap-1">
+        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-lightGray">
+          <UserOutlined className="text-xl" />
+        </div>
+        <CaretDownOutlined className="text-xs" />
+      </div>
+    </div>
+  );
+}
